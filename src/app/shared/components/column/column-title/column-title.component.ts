b7078d7f0c1a9dd99430f41de532c62251a305ef
= <grzegorz.kniazuk@gmail.com>
2019-04-27 12:14:44 +0200

    27.04 slide column title updates

--- src/app/shared/components/column/column-title/column-title.component.ts
+++ src/app/shared/components/column/column-title/column-title.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, EventEmitter, HostListener, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
 	selector: 'app-column-title',
@@ -6,18 +6,24 @@ import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, ViewChild
 	styleUrls: [ './column-title.component.scss' ],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ColumnTitleComponent {
+export class ColumnTitleComponent implements AfterViewInit {
 
 	@ViewChild('titleInput') private titleInput: ElementRef;
 	public columnTitle$: EventEmitter<string> = new EventEmitter<string>();
 	public columnTitle: string;
 	public dialogVisibility = true;
 
+	ngAfterViewInit() {
+		this.titleInput.nativeElement.focus();
+	}
+
+	@HostListener('document:keydown.enter')
 	public onSave(): void {
 		this.dialogVisibility = false;
 		this.columnTitle$.emit(this.columnTitle);
 	}
 
+	@HostListener('document:keydown.escape')
 	public onCancel(): void {
 		this.dialogVisibility = false;
 	}
