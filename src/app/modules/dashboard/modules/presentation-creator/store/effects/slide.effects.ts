dfd8ce8cc2ae12594d37b4f8f140e491bc81f089
= <grzegorz.kniazuk@gmail.com>
2019-05-01 17:01:23 +0200

    01.05 update

--- src/app/modules/dashboard/modules/presentation-creator/store/effects/slide.effects.ts
+++ src/app/modules/dashboard/modules/presentation-creator/store/effects/slide.effects.ts
@@ -1,28 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import {
-	AddSlideToPresentation,
-	RemoveSlide,
-	SlideActionsTypes,
-} from 'src/app/modules/dashboard/modules/presentation-creator/store/actions/slide.actions';
-import { map, tap } from 'rxjs/operators';
-import { RemoveSlideFromLibrary } from 'src/app/modules/dashboard/store/actions/library.actions';
+import { RemoveSlide, SlideActionsTypes } from 'src/app/modules/dashboard/modules/presentation-creator/store/actions/slide.actions';
+import { tap } from 'rxjs/operators';
 import { ToastService } from 'src/app/shared/services/toast.service';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class SlideEffects {
-
-	@Effect({ dispatch: true })
-	public addSlideToPresentation$ = this.actions$.pipe(
-		ofType<AddSlideToPresentation>(SlideActionsTypes.AddSlideToPresentation),
-		map((action: AddSlideToPresentation) => {
-			return new RemoveSlideFromLibrary({
-				slideId: action.payload.slide.id,
-			});
-		}),
-	);
 
 	@Effect({ dispatch: false })
 	public removeSlide$ = this.actions$.pipe(
